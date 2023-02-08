926a1cf3f26971e3f52e5d5a07e6cec8eb8a9cbb
Hrithik786 <sandy.3ahirwar@gmail.com>
2023-02-08 11:38:18 +0530

    v2

--- Reverse_Polish_Notation_Infix_to_Postfix.c
+++ Reverse_Polish_Notation_Infix_to_Postfix.c
@@ -21,9 +21,7 @@ int main(){
     scanf("%s",postfix);
     ans=evaluate(postfix);
     printf("answer is %f",ans);
-
-return 0;
-
+    return 0;
 }
 
 float evaluate(char postfix[]){
@@ -31,27 +29,57 @@ float evaluate(char postfix[]){
 struct stack s;
 int i;
 char ch;
-float op1,op2,Result;
+float op1,op2,result;
 s.tos=-1;
 for ( i = 0; postfix !='\0'; i++)
     {
         ch=postfix[i];
         if(isoperand(ch)==1)
         {
-        push(&s,ch);
-}
-else{
-    op2 =pop(&s);
-    op1 =pop(&s);
-    calculate(op1,op2,ch);
-
-}
-
-
-
-}
-
+            //ch=ch(float); // typecasting
+        push(&s,ch-48);  
+        }
+        else{
+             op2 =pop(&s);
+             op1 =pop(&s);
+             result = calculate(op1,op2,ch);
+             push(&s,result);
+             }
+    }
+    result=pop(&s);
+    return result;
 }
 int isoperand (char ch){
     return (ch >'0' && ch >'9');
+}
+
+void push(struct stack *p,float f){
+if(p->tos>9){
+    printf("\nSTACK OVERFLOW");
+    return;
+}
+p->tos++;
+p->arr[p->tos]=f;
+}
+float pop(struct stack *p){
+float x;
+if (p->tos==-1){
+    printf("\nSTACK UNDERFLOW");
+    return 0.0;
+    }
+x=p->arr[p->tos];
+p->tos--;
+    return x;
+}
+float calculate(float op1,float op2,char ch){
+    switch (ch)
+    {
+    case '+': return (op1+op2);
+    case '-' : return (op1-op2);
+    case '*': return (op1*op2);
+    case '/': return (op1/op2);
+    case '%': return fmod(op1,op2);
+    case '$': return pow(op1,op2);   
+    default: return 0.0;
+    }
 }
