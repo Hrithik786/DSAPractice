28a32fd4c29ce6abb247e376a05ae239189d5ea1
Hrithik786 <sandy.3ahirwar@gmail.com>
2023-02-08 10:59:12 +0530

    working on it

--- Reverse_Polish_Notation_Infix_to_Postfix.c
+++ Reverse_Polish_Notation_Infix_to_Postfix.c
@@ -1,1 +1,34 @@
 //Infix to Postfix
+#include<stdio.h>
+
+struct stack
+{
+    float arr[10];
+    int tos;
+};
+
+void push(struct stack *, float);
+float pop(struct stack *);
+int isoperand(char);
+float calculate(float,float,char);
+float evaluate(char[]);
+
+int main(){
+struct stack s;
+int i;
+char ch;
+float x;
+s.tos=-1;
+
+char postfix[20];
+printf("please enter correct Infix expression:\n");
+scanf("%s",postfix);
+
+
+
+
+
+
+    return 0;
+
+}
