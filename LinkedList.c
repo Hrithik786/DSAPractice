44504f3429293e923d5255efb4360f69523789db
Hrithik786 <sandy.3ahirwar@gmail.com>
2023-03-09 19:22:42 +0530

    fix some issues

--- LinkedList.c
+++ LinkedList.c
@@ -1,3 +1,5 @@
+//Singly LinkedList Program
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -6,11 +8,15 @@ struct node{
     struct node *next; //self referential structure
 };
 
-void append(struct node **, int);
+void append(struct node **, int);    //Time_complexity O(n)
 void display(struct node *);
-int searchnode(struct node *, int);
-void prepend(struct node **,int);
-void addatpos(struct node**,int,int);
+int searchnode(struct node *, int); //Time_complexity O(n) by implementing searching algo we reduce its Time Complexity
+void prepend(struct node **,int);     //Time_complexity O(1)
+void addatpos(struct node **,int,int); //Time_complexity O(n)
+void delfirst(struct node **);        //Time_complexity O(1)
+void dellast(struct node **);          //Time_complexity O(n)
+int delatpos(struct node **,int);      //Time_complexity O(n)
+
 
 int main()
 {
@@ -18,7 +24,7 @@ int main()
     struct node *start = NULL;
     
 do{
-    printf("\nHELLO BOSS\n\n1.Append\n2.Display\n3.Search Position of Node\n4.Prepend\n5.Add at Position\n");
+    printf("\n\n*********************HELLO BOSS*******************\n\n1.Append\n2.Display\n3.Search Position of Node\n4.Prepend\n5.Add at Position\n6.Delete_First_node\n7.Delete_Last_Node\n8.Add_at_Position\n");
     scanf(" %d",&ch);
     switch(ch)
     {
@@ -56,11 +62,25 @@ do{
     ch=1;
     break;
 
+    case 6:
+    delfirst(&start);
+    ch=1;
+    break;
+    
+    case 7:
+    dellast(&start);   
+    ch=1;
+    break;
+    
+    case 8:
+    ch=1;
+    break;
+
     default:
     printf("\nPlease Enter Valid Input");
     break;
     }
-}while (ch!=4);
+}while (ch!=8);
 return 0;
 }
 int searchnode(struct node *p,int x){
@@ -98,7 +118,7 @@ while (temp->next!=NULL){
     temp=temp->next;
 }
 temp->next=p;
-printf("\nAppended Element  Successfully",temp->data);
+printf("\nAppended Element Successfully",temp->data);
 }
 void display(struct node *p){
 if(p==NULL){
@@ -167,4 +187,32 @@ void addatpos(struct node **ps,int x,int pos){
         } 
         printf("\nList is Small, Position not found, Add some nodes then try Again,Thank You!!");
         return;
+}
+void delfirst(struct node **ps){
+    struct node *temp;
+    if(*ps==NULL){
+        printf("\nList is Empty");
+        return;
+    }
+    temp=(*ps)->next;
+    free(*ps);
+    *ps=temp;
+    free(temp);
+    printf("\nFirst Node Deleted Successfully");
+}
+void dellast(struct node **ps){
+    if (*ps==NULL)
+    {
+        printf("\nList is Empty");
+        return;
+    }
+    struct node *prev,*temp=NULL;
+    temp=*ps;
+    while(temp!=NULL){
+        prev=temp;
+        temp=temp->next;
+    }
+    free(temp);
+    prev->next=NULL;   
+    printf("\nLast Element Deleted Successfully = %d",(temp->data));
 }
