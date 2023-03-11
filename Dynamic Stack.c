6b37f37050f77dbdbe0909a71f6f339536072c0f
Hrithik786 <sandy.3ahirwar@gmail.com>
2023-03-11 15:36:19 +0530

    added

---
+++ Dynamic Stack.c
@@ -0,0 +1,16 @@
+struct stack{
+    int data;
+    struct stack *next;
+};
+
+void push(struct stack **,int);
+int pop(struct stack **);
+
+int main(){
+    
+    
+    return 0;
+}
+void push(struct stack **ptos,int x){
+
+}
