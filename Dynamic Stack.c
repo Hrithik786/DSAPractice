107ce8b2af1821434a299820bc2bf0b04eb388c7
Hrithik786 <sandy.3ahirwar@gmail.com>
2023-03-13 19:38:55 +0530

    DynamicStack

--- Dynamic Stack.c
+++ Dynamic Stack.c
@@ -12,12 +12,15 @@ void peek(struct stack *);
 
 int main(){
     struct stack *tos=NULL;
-    push(&tos,10);
+    push(&tos, 10);
     push(&tos,-98);
     push(&tos,780);
     push(&tos,50);
     peek(tos);
     
+    while(tos!=NULL)
+    printf("\npopped element = %d",pop(&tos));
+
     return 0;
 }
 void push(struct stack **ptos,int x){
@@ -37,6 +40,18 @@ void push(struct stack **ptos,int x){
     *ptos=p;
     printf("\nPushed %d successfully",x);
 
+}
+int pop(struct stack **ptos){
+    int x;
+    if(*ptos==NULL){
+        printf("\nStack Underflow");
+        return -1;
+    }
+    x=(*ptos)->data;
+    struct stack *temp = *ptos;
+    *ptos=(*ptos)->next;    
+    free(temp);    
+    return x;
 }
 void peek(struct stack *ptos){
     if (ptos==NULL){
